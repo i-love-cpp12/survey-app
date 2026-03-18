8fcd94810aedee453a6f6e6b68e4354ef98f13d0
moskalaoliwier <moskalaoliwier@szybinski.com>
2026-03-18 14:05:40 +0100

    --amend

--- backend/src/infrastructure/database/conn.php
+++ backend/src/infrastructure/database/conn.php
@@ -30,7 +30,7 @@ class DB
         }
         catch(PDOException $e)
         {
-            throw new Exception("Connection couldn't be made");
+            throw new Exception("Connection couldn't be established");
         }
     }
     public static function getConnection(): PDO
